6e7030f08942718fa2639ed42898901cfcdf035b
Emily Palaska <103355365+emily-palaska@users.noreply.github.com>
2024-01-27 20:25:06 +0200

    Add files via upload

--- v1.cu
+++ v1.cu
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+
+# define bSize 16 // block size definition for easy access
 
 void takeInput(const char *filename, int *array, int n);
-void printArray(int *array, int n);
+void printArray(int *array, long n);
 
-// CUDA kernel to perform vector ising step
-__global__ void isingStep(int *dArrayInput, int *dArrayOutput, int n) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+// CUDA kernel to perform ising step
+__global__ void isingStep(int *dArrayInput, int *dArrayOutput, long n) {
+    long idx = blockIdx.x * blockDim.x + threadIdx.x;
 	int buffer;
+	
     // Check if the thread index is within the array bounds
-    
     if (idx < n * n) {
-    	int i = idx % n;
-    	int j = idx / n;
-    	
+    	long i = idx % n;
+    	long j = idx / n;
+ 
         // add all neighboring values
 		buffer = dArrayInput[(i - 1 + n) % n + j * n];		//left
 		buffer += dArrayInput[(i + 1) % n + j * n];			//right
@@ -26,41 +29,55 @@ __global__ void isingStep(int *dArrayInput, int *dArrayOutput, int n) {
     }
 }
 
-int main() {
-	int n = 7; // dimension
-	const char *filename = "input.txt"; //input file name
-	int k = 10; // number of steps
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		printf("Invalid arguments\n");
+		return 1;
+	}
+	
+	const char *filename = "input.txt"; // input file name
+	long n = 10; 						// dimension
+	int k = atoi(argv[1]); 				// number of steps taken as argument
 	int counter = 0;
+	struct timeval t1, t2;				// variables for elapsed time
 	
-	// MEMORY ALLOCATION
+	///////////////////////	
+	// MEMORY ALLOCATION //
+	///////////////////////
 	
-	// host array initialization
-	int *hArray = (int *)malloc(n * n * sizeof(int));
-	
+	// host
+	int *hArray = (int *)malloc(n * n * sizeof(int));	
 	if(!hArray) {
 		perror("Memory allocation failed");
 		return 1;
 	}	
-		
-	// device array initialization	
-	int *dArray1;
-	int *dArray2;
 	
+	// host	
+	int *dArray1, *dArray2;
 	cudaMalloc((void **)&dArray1, n * n * sizeof(int));
 	cudaMalloc((void **)&dArray2, n * n * sizeof(int));
 	
-	// INITIAL STATE
+	///////////////////
+	// INITIAL STATE //
+	///////////////////
+	
 	takeInput(filename, hArray, n);
 	printf("Initial state:\n");
 	printArray(hArray, n);
+	printf("\n\nFinal state:\n");
 	
-	// ALGORITHM APPLICATION
+	///////////////
+	// ALGORITHM //
+	///////////////
 	
+	// start timer
+	gettimeofday(&t1, NULL);
+		
 	// copy host vectors to device
     cudaMemcpy(dArray1, hArray, n * n * sizeof(int), cudaMemcpyHostToDevice);
 	
-	// Define the grid and block dimensions
-    int blockSize = 256;
+	// define the grid and block dimensions
+    int blockSize = bSize;
     int gridSize = (n * n + blockSize - 1) / blockSize;
     
     while(counter < k) {
@@ -71,28 +88,39 @@ int main() {
     	isingStep<<<gridSize, blockSize>>>(dArray2, dArray1, n);
     	counter++;
 	}
-    // Copy the result back to the host
+	
+    // copy the result back to the host
     if(counter % 2)    
-    	cudaMemcpy(hArray, dArray1, n * n * sizeof(int), cudaMemcpyDeviceToHost);
-    else
     	cudaMemcpy(hArray, dArray2, n * n * sizeof(int), cudaMemcpyDeviceToHost);
+    else
+    	cudaMemcpy(hArray, dArray1, n * n * sizeof(int), cudaMemcpyDeviceToHost);
 	
-	// RESULT PRINTING
-	printf("\nResult:\n");
+	// stop timer
+	gettimeofday(&t2, NULL);
+	double elapsedTime;
+	elapsedTime = (t2.tv_sec - t1.tv_sec);      			// sec
+    elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000000.0;   // us to sec
+	
+	////////////////
+	// DISPLAYING //
+	////////////////
+	
+	//printf("\nResult:\n");
 	printArray(hArray, n);
+	//printf("Successful temination for: n = %d k = %d\nTime elapsed: %.4f seconds\n", n, k, elapsedTime);
+	
+	//////////////////////////
+	// MEMORY DE-ALLOCATION //
+	//////////////////////////
 	
 	
-	// MEMORY DE-ALLOCATION
-	
-	// Host
-	free(hArray);
-
-	// Device
-    cudaFree(dArray1);
+	free(hArray);		// host
+    cudaFree(dArray1);	// device
     cudaFree(dArray2);
 	return 0;
 }
 
+// function that takes the initial values of the grid from a txt file
 void takeInput(const char *filename, int *array, int n) {
 	FILE *file = fopen(filename, "r");
 	if(file == NULL) {
@@ -109,11 +137,15 @@ void takeInput(const char *filename, int *array, int n) {
 	return;
 }
 
-void printArray(int *array, int n) {
+// auxialiary function to print the grid - mainly for checks
+void printArray(int *array, long n) {
 	for(int i = 0; i < n; i++) {
-		for(int j = 0; j < n; j++)
-			printf("%d\t", array[i * n + j]);
+		for(int j = 0; j < n; j++) {
+				if(array[i * n + j] >= 0) printf("+");
+				printf("%d ", array[i * n + j]);
+			}
 		printf("\n");
 	}
 	return;
 }
+
